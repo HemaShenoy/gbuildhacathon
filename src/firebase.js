47e9b2eb197b6f0b3f3c4ff2f15810837e41ee03
HemaShenoy <122464897+HemaShenoy@users.noreply.github.com>
2024-04-04 10:19:09 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,6 @@ import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
 
 
-import { getFirestore } from 'firebase/firestore';
-
-
 
 
 
@@ -23,13 +20,14 @@ const firebaseConfig = {
 };
 
 
+
+
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-
-export { app, analytics, auth, db };
+export { app, analytics, auth };
 
 
 
